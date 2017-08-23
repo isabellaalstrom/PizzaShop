6c199c310648550003b461b9a0274c72276f47b7
isabellaalstrom <isabella.alstrom@gmail.com>
2017-08-23 10:05:48 +0200

    added cart

---
+++ PizzaShop/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaShop.Models
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        public Cart Cart { get; set; }
+        public int CartId { get; set; }
+        public Dish Dish { get; set; }
+        public int DishId { get; set; }
+        public int Quantity { get; set; }
+        public List<CartItemIngredient> CartItemIngredients { get; set; }
+    }
+}
