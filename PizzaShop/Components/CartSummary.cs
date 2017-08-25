5327bb7db47827fc8545c27f43c69a34b106a998
isabellaalstrom <isabella.alstrom@gmail.com>
2017-08-25 10:20:45 +0200

    Added viewcomponent for cart. Added images of pizzas

---
+++ PizzaShop/Components/CartSummary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Models;
+using PizzaShop.Models.CartViewModels;
+
+namespace PizzaShop.Components
+{
+    public class CartSummary : ViewComponent
+    {
+        public readonly Cart _cart;
+
+        public CartSummary(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var items = _cart.GetCartItems();
+            var items = new List<CartItem>() { new CartItem(), new CartItem() };
+            _cart.CartItems = items;
+            var cartVM = new CartViewModel
+            {
+                Cart = _cart,
+                TotalAmount = _cart.GetCartTotal()
+            };
+            return View(cartVM);
+        }
+    }
+}
