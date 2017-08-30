6779a762ddb2a4c791c722f23ab8f3bff9f1f3b6
isabellaalstrom <isabella.alstrom@gmail.com>
2017-08-30 08:25:31 +0200

    Added price to ingredient and updates view accordingly

--- PizzaShop/Entities/Ingredient.cs
+++ PizzaShop/Entities/Ingredient.cs
@@ -9,6 +9,7 @@ namespace PizzaShop.Entities
         public int IngredientId { get; set; }
         [DisplayName("Ingredient")]
         public string IngredientName { get; set; }
+        public int Price { get; set; }
         public List<DishIngredient> DishIngredients { get; set; }
         public List<CartItemIngredient> CartItemIngredients { get; set; }
     }
